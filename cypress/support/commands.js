e69ab31d5ea7278d682589fea2dc77527825bebe
rafael-isidro <rafa-isidro@hotmail.com>
2025-03-01 18:22:36 -0300

    refact: Ajustando nome de teste para carrinho

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
 import basePage from "./pages/base.page.js";
 import loginPage from "./pages/login.page.js";
-import produtoPage from "./pages/produto.page.js";
+import carrinhoPage from "./pages/carrinho.page.js";
 import checkoutPage from "./pages/checkout.page.js";
